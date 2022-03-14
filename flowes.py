169bf319fe188edae5949270b0ad9f3e626bd948
AhmadJanuar1 <88914702+AhmadJanuar1@users.noreply.github.com>
2022-03-14 20:27:42 +0700

    Create flowes.py

---
+++ flowes.py
@@ -0,0 +1,19 @@
+from turtle import *
+from colorsys import *
+
+hue = 0.8
+bgcolor("black")
+speed(0)
+
+for i in range(264):
+    col = hsv_to_rgb(hue, 1, 1)
+    hue += 0.005
+    color(col, col)
+    begin_fill()
+    circle(270 - i, 90)
+    lt(90)
+    circle(270 - i, 90)
+    lt(10)
+    end_fill()
+
+donr()
